ff451e47a859c3942fe5cdc4132bd51ef6728f44
Hector <cuevash.sp@gmail.com>
2018-07-11 18:56:07 +0200

    Added phones mocked data

--- server/app.js
+++ server/app.js
@@ -1,26 +1,7 @@
 const express = require('express')
 
 // Mock-up phones's data
-const DataPhones = [
-  {
-    id: '1',
-    title: 'Samsung S6',
-    description: 'Mobile from samsung, light, excellent camera, long batery',
-    url: 'localhost:5000/media/samsung_s6.jpeg',
-    color: 'white',
-    price: 200,
-    priceUnit: 'euro'
-  },
-  {
-    id: '2',
-    title: 'Iphone 7',
-    description: 'Mobile from Apple, light, excellent camera, long batery',
-    url: 'localhost:5000/media/Iphone7.jpg',
-    color: 'white',
-    price: 300,
-    priceUnit: 'euro'
-  }
-]
+const DataPhones = require('./mock/phoneDat')
 
 // Create server
 const app = express()
---
+++ server/mock/phoneDat.js
@@ -0,0 +1,77 @@
+// Mock-up phones's data
+const DataPhones = [
+  {
+    id: '1',
+    title: 'Samsung S6',
+    description: 'Mobile from samsung, light, excellent camera, long batery',
+    imageUrl: 'http://localhost:5000/media/samsung_s6.jpeg',
+    color: 'white',
+    price: 200,
+    priceUnit: 'euro'
+  },
+  {
+    id: '2',
+    title: 'Iphone 7',
+    description: 'Mobile from Apple, light, excellent camera, long batery',
+    imageUrl: 'http://localhost:5000/media/Iphone7.jpg',
+    color: 'white',
+    price: 300,
+    priceUnit: 'euro'
+  },
+  {
+    id: '3',
+    title: 'Samsung S6',
+    description: 'Mobile from samsung, light, excellent camera, long batery',
+    imageUrl: 'http://localhost:5000/media/samsung_s6.jpeg',
+    color: 'white',
+    price: 200,
+    priceUnit: 'euro'
+  },
+  {
+    id: '4',
+    title: 'Iphone 7',
+    description: 'Mobile from Apple, light, excellent camera, long batery',
+    imageUrl: 'http://localhost:5000/media/Iphone7.jpg',
+    color: 'white',
+    price: 300,
+    priceUnit: 'euro'
+  },
+  {
+    id: '5',
+    title: 'Samsung S6',
+    description: 'Mobile from samsung, light, excellent camera, long batery',
+    imageUrl: 'http://localhost:5000/media/samsung_s6.jpeg',
+    color: 'white',
+    price: 200,
+    priceUnit: 'euro'
+  },
+  {
+    id: '6',
+    title: 'Iphone 7',
+    description: 'Mobile from Apple, light, excellent camera, long batery',
+    imageUrl: 'http://localhost:5000/media/Iphone7.jpg',
+    color: 'white',
+    price: 300,
+    priceUnit: 'euro'
+  },
+  {
+    id: '7',
+    title: 'Samsung S6',
+    description: 'Mobile from samsung, light, excellent camera, long batery',
+    imageUrl: 'http://localhost:5000/media/samsung_s6.jpeg',
+    color: 'white',
+    price: 200,
+    priceUnit: 'euro'
+  },
+  {
+    id: '8',
+    title: 'Iphone 7',
+    description: 'Mobile from Apple, light, excellent camera, long batery',
+    imageUrl: 'http://localhost:5000/media/Iphone7.jpg',
+    color: 'white',
+    price: 300,
+    priceUnit: 'euro'
+  }
+]
+
+module.exports = DataPhones
